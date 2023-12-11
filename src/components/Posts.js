6a140eb935d92fcd51968dd43a3ee26ad2067703
tracycheung98 <tracycheung98@gmail.com>
2023-12-11 17:03:56 -0500

    added like button

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHeart as solidHeart} from '@fortawesome/free-solid-svg-icons';
+import {faHeart as regularHeart} from '@fortawesome/free-regular-svg-icons';
 import { getDatabase, ref, onValue } from "firebase/database";
 import Card from 'react-bootstrap/Card';
 
+
 export default function Posts(props) {
     const [posts, setPosts] = useState([]);
+    const [likedPosts, setLikedPosts] = useState([]);
 
     useEffect(() => {
         const db = getDatabase();
         const postsRef = ref(db, "posts");
 
-        const unsubscribe = onValue(postsRef, (snapshot) => {
+        const unsubscribePosts = onValue(postsRef, (snapshot) => {
             const postsData = snapshot.val();
             if (postsData) {
                 const postsArray = Object.entries(postsData).map(([postId, postData]) => ({
@@ -34,17 +39,26 @@ export default function Posts(props) {
                         );
                     });
                 }
-
                 setPosts(filteredPosts);
+                // console.log("filteredPosts" + JSON.stringify(filteredPosts))
             } else {
                 setPosts([]);
             }
         });
 
-        return () => unsubscribe();
-    }, [props.isLoggedIn, props.currentUser, props.friendList]);
+        return () => unsubscribePosts();
+    }, [props.isLoggedIn, props.currentUser, props.friendList, likedPosts]);
 
     const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+
+    const handleLike = (postId) => {
+        console.log("like button clicked",postId);
+        if (likedPosts.includes(postId)) {
+            setLikedPosts(likedPosts.filter((id) => id !== postId));
+        } else {
+            setLikedPosts([...likedPosts, postId]);
+        }
+    };
 
     return (
         <div>
@@ -52,13 +66,20 @@ export default function Posts(props) {
             <div className="posts">
                 {posts.map((post) => (
                     <Card key={post.id}>
-                        <Card.Img  src={post.photoUrl} />
+                        <Card.Img src={post.photoUrl} />
                         <Card.Body>
                             {/* <Card.Text> */}
+                
+                            <button className="likeButton" onClick={() => handleLike(post.id)}>
+                            <FontAwesomeIcon icon={likedPosts.includes(post.id) ? solidHeart : regularHeart} />
+                            
+
+                            </button>
+
                             <span className="postUser"> {post.username}  &nbsp; </span>
                             <div dangerouslySetInnerHTML={{ __html: post.caption.replace(/\n/g, '<br />') }} style={{ whiteSpace: 'pre-line' }} />
                             <p className="postTime"> {new Date(post.timestamp).toLocaleString('en-US', options)}</p>
-                               
+
                             {/* </Card.Text> */}
 
                         </Card.Body>
